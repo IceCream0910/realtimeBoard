e89174e99704221b1d6babf9d9c700bdaa97eb3c
IceCream0910 <44636839+IceCream0910@users.noreply.github.com>
2020-06-30 09:52:23 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var app = require('express')();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT;
 var path = require('path');
 var bodyParser = require('body-parser'); // for reading POSTed form data into `req.body`
 
